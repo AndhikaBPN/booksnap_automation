d53721affb2dcc8d6dd883a1f47a9be4c216b4fa
Andhika Nusantara <dikanusantara00@gmail.com>
2026-01-12 17:09:11 +0700

    feat: add homepage testcase

--- tests/ui/page/homePage.spec.ts
+++ tests/ui/page/homePage.spec.ts
@@ -1,6 +1,7 @@
-import {test, expect} from '@playwright/test';
+import {test} from '@playwright/test';
 import { HomePageFlow } from '../../flows/page/HomePageFlow';
 import { LoginFlow } from '../../flows/auth/LoginFlow';
+import { Generator } from '../../helper/Generator';
 
 test.describe('Home Page', () => {
     let homePageFlow: HomePageFlow;
@@ -43,5 +44,34 @@ test.describe('Home Page', () => {
 
     test('Click comment button', async ({page}) => {
         await homePageFlow.clickCommentButton();
+    });
+
+    test('Enter the comment', async ({page}) => {
+        const comment = Generator.randomStringGenerator();
+        await homePageFlow.enterComment(comment);
+    });
+
+    test('Share the book', async ({page}) => {
+        await homePageFlow.shareTheBook();
+    });
+
+    test('Click save the book', async ({page}) => {
+        await homePageFlow.clickSaveTheBook();
+    });
+
+    test('Click my library page', async ({page}) => {
+        await homePageFlow.clickMyLibraryPage();
+    });
+
+    test('Search for saved books', async ({page}) => {
+        await homePageFlow.searchForSavedBooks('Where the Crawdads Sing');
+    });
+
+    test.skip('Filter by recently added', async ({page}) => {
+        await homePageFlow.filterByRecentlyAdded();
+    });
+
+    test('Click search button', async ({page}) => {
+        await homePageFlow.clickSearchButton();
     });
 });
